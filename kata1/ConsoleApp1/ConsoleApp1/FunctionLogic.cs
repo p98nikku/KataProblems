826fb8e1aaca8f0954bf3bb3783c752388171e3a
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-05-10 15:01:23 +0530

    completed first kata using tuples

--- kata1/ConsoleApp1/ConsoleApp1/FunctionLogic.cs
+++ kata1/ConsoleApp1/ConsoleApp1/FunctionLogic.cs
@@ -6,15 +6,14 @@ namespace ConsoleApp1
 {
     public class FunctionLogic
     {
-       
-
         public static Tuple<int, int> NbrOfLaps()
         {
             Console.WriteLine("Enter the value of first lap:");
-            int x = Convert.ToInt32(Console.ReadLine());
+            int LengthofFirstLap = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the value of second lap:");
-            int y = Convert.ToInt32(Console.ReadLine());
-            int num1 = x, num2 = y;
+            int LengthofSecondLap = Convert.ToInt32(Console.ReadLine());
+            int num1 = LengthofFirstLap;
+            int num2 = LengthofSecondLap;
             int lcm;
             while (num1 != num2)
             {
@@ -27,12 +26,12 @@ namespace ConsoleApp1
                     num2 = num2 - num1;
                 }
             }
-            lcm = (x * y) / num1;
-            int NoOfRoundForA, NoOfRoundForB;
-
-            NoOfRoundForA = lcm / x;
-            NoOfRoundForB = lcm / y;
-            return new Tuple<int, int>(NoOfRoundForA, NoOfRoundForB);
+            lcm = (LengthofFirstLap * LengthofSecondLap) / num1;
+            int RoundCompletedbyA = 0;
+            int RoundCompletedbyB = 0;
+            RoundCompletedbyA = lcm / LengthofFirstLap;
+            RoundCompletedbyB = lcm / LengthofSecondLap;
+            return new Tuple<int, int>(RoundCompletedbyA, RoundCompletedbyB);
         }
     }
 }
