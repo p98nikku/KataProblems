7e61695ca474bc7e631dc3069fd8e4273d4b2cc3
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-05-11 00:20:33 +0530

    maintained the structure

---
+++ kata3/DeleteOccurances/DeleteOccurances/FunctionLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteOccurances
+{
+    public class FunctionLogic
+    {
+        public static void EntryLogic()
+        {
+            int[] array = DeleteNth(new int[] { 20, 37, 20, 21 }, 1);
+            int[] array2 = new int[4];
+            Console.WriteLine("Output: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            List<int> temporaryArray = new List<int>();
+            return Result;
+        }
+    }
+}
--- kata3/DeleteOccurances/DeleteOccurances/Program.cs
+++ kata3/DeleteOccurances/DeleteOccurances/Program.cs
@@ -2,11 +2,12 @@
 
 namespace DeleteOccurances
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FunctionLogic.EntryLogic();
         }
+        
     }
 }
