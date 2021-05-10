eb8b88f7571cac2a79ddbc2342710542911930c8
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-05-10 14:36:48 +0530

    started the problems

---
+++ kata1/ConsoleApp1/ConsoleApp1/FunctionLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class FunctionLogic
+    {
+       
+
+        public static Tuple<int, int> NbrOfLaps()
+        {
+            Console.WriteLine("Enter the value of first lap:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of second lap:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int num1 = x, num2 = y;
+            int lcm;
+            while (num1 != num2)
+            {
+                if (num1 > num2)
+                {
+                    num1 = num1 - num2;
+                }
+                else
+                {
+                    num2 = num2 - num1;
+                }
+            }
+            lcm = (x * y) / num1;
+            int NoOfRoundForA, NoOfRoundForB;
+
+            NoOfRoundForA = lcm / x;
+            NoOfRoundForB = lcm / y;
+            return new Tuple<int, int>(NoOfRoundForA, NoOfRoundForB);
+        }
+    }
+}
